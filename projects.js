e500ddae123ca0e398eef49c5b8f39d40d97288e
brookeojemen <brooke.ojemen@gmail.com>
2026-08-11 16:19:54 -0400

    Set image property to an empty string
    Initialize image property with an empty string.

--- projects.js
+++ projects.js
@@ -59,7 +59,7 @@ window.PROJECTS = [
     date: "2026-07",
     github: "https://github.com/brookeojemen/HousingPrediction",
     caseStudy: "project-details/housing.html",
-    image: ,
+    image: "",
     imageAlt: "Housing Price Prediction regression project currently in progress",
     metrics: ["1,460 rows", "81 columns", "Regression"]
   }  ,{
